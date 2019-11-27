fe14cf7766856fee7e96470791cdc97b97962c7b
Erik Aceiro Antonio <aceiro@gmail.com>
2019-11-26 21:45:07 -0300

    Refatoracao para classe

---
+++ services/servicebase.py
@@ -0,0 +1,29 @@
+import requests
+import json
+from usecases.configusecases import ConfigUseCases
+
+class ServiceBase:
+    def __init__(self):
+        pass
+    def buildRouteAndGetHttp(self, useCase = ConfigUseCases(), routeApi = ""):
+        response = {}
+        try:
+            endpoint = str(useCase.meliApi.api['endpoint'])            
+            site_id  = str(useCase.meliApi.api['site_id'])
+            routeApi = routeApi.replace("${ENDPOINT}", endpoint)
+            routeApi = routeApi.replace("${SITE_ID}", site_id)
+            #######################################
+            # chama ML API remota
+            #######################################
+            categories = requests.get(routeApi).text
+            
+            #######################################
+            # monta o json
+            #######################################
+            message = json.dumps(categories, sort_keys=True)            
+            response['status']  = 200
+            response['message'] = message
+        except:
+            response = {'status':500, 'message': 'Fail request'}
+            print("Error to process URL")
+        return response
