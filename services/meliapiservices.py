bc4d300b39d7672fa61ed637ab731017d3dbce20
Erik Aceiro Antonio <aceiro@gmail.com>
2019-11-26 21:41:12 -0300

    Refatoracao para classe via Servicebase

--- services/meliapiservices.py
+++ services/meliapiservices.py
@@ -2,19 +2,24 @@ import requests
 import json
 from usecases.configusecases import ConfigUseCases
 
-class MeLiApiService:
+class ServiceBase:
     def __init__(self):
         pass
-
-    def categories(self, useCase = ConfigUseCases()):
+    def buildRouteAndGetHttp(self, useCase = ConfigUseCases(), routeApi = ""):
         response = {}
         try:
-            endpoint = str(useCase.meliApi.api['endpoint'])
-            routeCategories = str(useCase.meliApi.api['categories'])
-            site_id = str(useCase.meliApi.api['site_id'])
-            routeCategories = routeCategories.replace("${ENDPOINT}", endpoint)
-            routeCategories = routeCategories.replace("${SITE_ID}", site_id)
-            categories = requests.get(routeCategories).text
+            endpoint = str(useCase.meliApi.api['endpoint'])            
+            site_id  = str(useCase.meliApi.api['site_id'])
+            routeApi = routeApi.replace("${ENDPOINT}", endpoint)
+            routeApi = routeApi.replace("${SITE_ID}", site_id)
+            #######################################
+            # chama ML API remota
+            #######################################
+            categories = requests.get(routeApi).text
+            
+            #######################################
+            # monta o json
+            #######################################
             message = json.dumps(categories, sort_keys=True)            
             response['status']  = 200
             response['message'] = message
@@ -23,18 +28,14 @@ class MeLiApiService:
             print("Error to process URL")
         return response
 
+class MeLiApiService(ServiceBase):
+    def __init__(self):
+        pass
+
+    def categories(self, useCase = ConfigUseCases()):
+        routeCategories = str(useCase.meliApi.api['categories'])
+        return self.buildRouteAndGetHttp(useCase, routeCategories)
+
     def sites(self, useCase = ConfigUseCases()):
-        response = {}
-        try:
-            endpoint = str(useCase.meliApi.api['endpoint'])
-            routeSites = str(useCase.meliApi.api['sites'])
-            site_id = str(useCase.meliApi.api['site_id'])
-            routeSites = routeSites.replace("${ENDPOINT}", endpoint)
-            routeSites = routeSites.replace("${SITE_ID}", site_id)
-            message = json.dumps(requests.get(routeSites).text, indent=4, sort_keys=True)
-            response['status']  = 200
-            response['message'] = message
-        except:
-            response = {'status':500, 'message': 'Fail request'}
-            print("Error to process URL")
-        return response
+        routeSites = str(useCase.meliApi.api['sites'])
+        return self.buildRouteAndGetHttp(useCase, routeSites)
